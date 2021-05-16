459c51e56d199cd7b4e6f852b9945006c1e0b236
Oleg <Shevchenko.oleg.1992@gmail.com>
2021-05-16 17:16:53 +0300

    test git

---
+++ js/script.js
@@ -0,0 +1,82 @@
+"use strict";
+
+let number = 4.5;
+
+console.log(-4/0);
+
+console.log( 'str' * 9 );
+
+const persone = `5`;
+
+const bool = false;
+
+// console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+	name: "Jhone",
+	age: 25,
+	isMarrid: false
+};
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+let arr = ['plum.png', 'oranje.jpg', 6, 'apple.bmp', {}, [] ];
+console.log(arr[1]);
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log( typeof(answers) );
+// console.log( typeof(null) );
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет,  ${user}`);
+
+console.log('arr' + ' - object');
+
+console.log(4 + ' - object');
+
+console.log(4 + '5');
+
+let incr = 10,
+		decr = 10;
+
+	// ++incr;
+	// --decr;
+
+	console.log(incr++);
+	console.log(--decr);
+
+	console.log(5 % 2);
+
+	
+	console.log(2 * 4 === 8);
+
+	const isChecked = false,
+				isClose = false;
+
+	console.log(isChecked || !isClose);
+	console.log(isChecked && isClose);
+
+	console.log(2 + 2 * 2 === 8);
+
